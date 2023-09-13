3e362ad26b703084e6e374701e7240950573f721
Aliona Barash <aliona.barash166@gmail.com>
2023-09-13 14:58:15 +0300

    Set player movement limits

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private float _xRightLimit, _xLeftLimit;
+    [SerializeField]
+    private float _yUpperLimit, _yLowerLimit;
 
     private Rigidbody2D _rigidbody;
     private Vector2 _input;
@@ -18,6 +22,22 @@ public class Player : MonoBehaviour
     void Update()
     {
         _input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * _speed;
+
+
+        if (transform.position.x > _xRightLimit)
+        {
+            transform.position = new Vector2(_xLeftLimit, transform.position.y);
+        }
+        else if (transform.position.x < _xLeftLimit)
+        {
+            transform.position = new Vector2(_xRightLimit, transform.position.y);
+        }
+
+        if (transform.position.y > _yUpperLimit || transform.position.y < _yLowerLimit)
+        {
+            transform.position = new Vector2(transform.position.x,
+                Mathf.Clamp(transform.position.y, _yLowerLimit, _yUpperLimit));
+        }
     }
 
     void FixedUpdate()
