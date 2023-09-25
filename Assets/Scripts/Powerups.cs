5dcbb93520ceda5abfb712fc0280b233cdb171d8
Aliona Barash <aliona.barash166@gmail.com>
2023-09-26 00:07:00 +0300

    Set up power-ups appearance

---
+++ Assets/Scripts/Powerups.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerups : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4;
+
+    private Rigidbody2D _rigidbody;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + Vector2.down * _speed * Time.fixedDeltaTime);
+    }
+}
